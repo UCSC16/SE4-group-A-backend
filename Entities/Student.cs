c78914869a691c14dc1bf3123e4dcd401b7cc1e6
ddhanushka <dimuthudhanushka8@gmail.com>
2023-02-11 12:49:53 +0530

    View students basic code

---
+++ Entities/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SE4_group_A_backend.Entities;
+
+public partial class Student
+{
+    public string StudentId { get; set; } = null!;
+
+    public string StudentName { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+
+    public string ContactNumber { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public DateTime Dob { get; set; }
+
+    public string? GuardianName { get; set; }
+
+    public string? GuardianContact { get; set; }
+
+    public DateTime AdmissionDate { get; set; }
+
+    public DateTime? GraduationDate { get; set; }
+
+    public string CurrentGrade { get; set; } = null!;
+
+    public virtual ICollection<Achievement> Achievements { get; } = new List<Achievement>();
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SE4_group_A_backend.Entities;
+
+public partial class User
+{
+    public string Username { get; set; } = null!;
+
+    public string Pasword { get; set; } = null!;
+
+    public string? Token { get; set; }
+
+    public DateTime? LastLoggedIn { get; set; }
+}
