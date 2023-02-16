48192a7819d60bcee4f197635482d9e3b70f5ba5
M.H.D.S Jayalath <dinukasandaruwan.ds@gmail.com>
2023-02-16 22:00:32 +0530

    Auto Increment for Student Id and Achievement Id Added

--- Entities/Achievement.cs
+++ Entities/Achievement.cs
@@ -5,7 +5,7 @@ namespace SE4_group_A_backend.Entities;
 
 public partial class Achievement
 {
-    public string AchievementId { get; set; } = null!;
+    public string? AchievementId { get; set; } = null!;
 
     public string StudentId { get; set; } = null!;
 
